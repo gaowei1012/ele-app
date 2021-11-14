23608d8936bfd14c63936ce358154859ed7920c5
gaowei1012 <gyb2020018@163.com>
2021-11-14 13:12:46 +0800

    add antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,13 @@
 const { override, fixBabelImports, addWebpackAlias, addDecoratorsLegacy, addWebpackPlugin } = require('customize-cra')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const WebpackBar = require('webpackbar')
 const path = require('path')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
   }),
   addWebpackAlias({
     // eslint-disable-next-line no-useless-computed-key
@@ -14,6 +15,7 @@ module.exports = override(
   }),
   addDecoratorsLegacy(),
   addWebpackPlugin(
-    new ProgressBarPlugin(), // webpack 打包进度条
+    // new ProgressBarPlugin(), // webpack 打包进度条
+    new WebpackBar()
   )
 )
