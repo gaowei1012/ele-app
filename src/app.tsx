cac2bd11e450177b72b6ad045db1864c87f3e607
gaowei1012 <gyb2020018@163.com>
2021-11-14 17:07:57 +0800

    fix folder

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import loadable from './hooks/useLoadble'
 
 const BaseLayout = loadable(
-  () => import(/* webpackChunkName: 'default' */ "./container/BaseLayout")
+  () => import(/* webpackChunkName: 'default' */ "./containers/BaseLayout")
 );
 
 const App = () => {
